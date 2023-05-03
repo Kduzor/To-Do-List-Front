824fd8d853988ccd8835f745219c091cd7be435c
cadu <kduzor@gmail.com>
2023-05-03 18:01:13 -0300

    cadu

--- src/app/shared/servicos/atividade.service.ts
+++ src/app/shared/servicos/atividade.service.ts
@@ -15,25 +15,25 @@ export class AtividadeService {
   private API_URL= `${environment.API_URL}/atividade`;
 
  //concluidoOuNao = filtro a ser aplicado na listagem
-  listarAtividade (concluidoOuNao: boolean): Observable<Atividade[]>{          
+  public listarAtividade (concluidoOuNao: boolean): Observable<Atividade[]>{          
     const link = `${this.API_URL}/listar-atividade`;       
     return this.http.post<Atividade[]>(link,{estadoDaAtividade: concluidoOuNao});
   }
 
-  //recebe  o estado da conclusao da atividade para ser alterada
-  atualizarAtividade(id: Number | undefined , concluidoOuNao:boolean): Observable<Atividade[]>{   
+  //recebe o estado da atividade para ser alterada
+  public atualizarAtividade(id: Number | undefined , concluidoOuNao:boolean): Observable<Atividade[]>{   
     const link = `${this.API_URL}/atualizar-atividade`;       
     return this.http.patch<Atividade[]>(link,{id: id , estadoDaAtividade: concluidoOuNao});
   }
 
-
-  excluir(id: number | undefined ): Observable<Atividade[]>{   
+  // recebe somente o id para ser excluido
+  public excluir(id: number | undefined ): Observable<Atividade[]>{   
     const link = `${this.API_URL}/deletar/${id}`;      
     return this.http.delete<Atividade[]>(link);
   }
 
    //recebendo id altera a descrição da atividade //sem id cria atividade
-  registrar(descricao: String, id?: number): Observable<Atividade[]>{      
+   public registrar(descricao: String, id?: number): Observable<Atividade[]>{      
     const link = `${this.API_URL}/cadastrar`;           
     return this.http.post<Atividade[]>(link,{descricao: descricao,id: id});    
   }
